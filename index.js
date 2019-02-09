05058e4c3e36271ff430c574ce5b498080f4c72c
William Holmes <holmes.william@gmail.com>
2019-02-09 21:50:27 +0000

    comment unused code

--- index.js
+++ index.js
@@ -53,41 +53,41 @@ slackEvents.on('link_shared', linkEvent);
 
 slackEvents.on('error', errorEvent);
 
-//grunt
-module.exports = function(grunt) {
+// //grunt
+// module.exports = function(grunt) {
  
-  // Project configuration.
-  grunt.initConfig({
-    sass: {
-      dist: {
-        files: {
-          'sass/styles.scss': 'css/styles.css'
-        }
-      }
-    },
+//   // Project configuration.
+//   grunt.initConfig({
+//     sass: {
+//       dist: {
+//         files: {
+//           'sass/styles.scss': 'css/styles.css'
+//         }
+//       }
+//     },
  
-    autoprefixer: {
-      single_file: {
-        options: {
-          browsers: ['last 2 version', 'ie 8', 'ie 9']
-        },
-        src: 'css/styles.css',
-        dest: 'css/styles.css'
-      },
-    },
+//     autoprefixer: {
+//       single_file: {
+//         options: {
+//           browsers: ['last 2 version', 'ie 8', 'ie 9']
+//         },
+//         src: 'css/styles.css',
+//         dest: 'css/styles.css'
+//       },
+//     },
  
-    watch: {
-      sass: {
-        files: 'sass/*.scss',
-        tasks: ['sass', 'autoprefixer'],
-      }
-    },
-  });
+//     watch: {
+//       sass: {
+//         files: 'sass/*.scss',
+//         tasks: ['sass', 'autoprefixer'],
+//       }
+//     },
+//   });
  
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+//   grunt.loadNpmTasks('grunt-autoprefixer');
+//   grunt.loadNpmTasks('grunt-contrib-sass');
+//   grunt.loadNpmTasks('grunt-contrib-watch');
  
-  // Default task
-  grunt.registerTask('default', ['watch']);
-};
+//   // Default task
+//   grunt.registerTask('default', ['watch']);
+// };
