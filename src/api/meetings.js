2fcc7ccb9200ce5e18127b35cd7506158eb75458
ceiran316 <x00132492@gmail.com>
2019-03-20 16:26:57 +0000

    bug fix 2

--- src/api/meetings.js
+++ src/api/meetings.js
@@ -2,10 +2,8 @@ const db = require('../../db');
 const constants = require('../../src/constants')
 
 const api = {
-    meetings: {
-        create: obj => {
-            return db.updateDOC(constants.db.DOCS.MEETINGS, { [obj.id]: obj })
-        }
+    create: obj => {
+        return db.updateDOC(constants.db.DOCS.MEETINGS, { [obj.id]: obj })
     }
 }
 
