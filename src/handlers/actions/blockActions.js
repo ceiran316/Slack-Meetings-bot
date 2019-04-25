2df61776914e1d05591fba954aaba8459bb751f4
ceiran316 <x00132492@gmail.com>
2019-04-25 23:34:54 +0100

    block actions update

--- src/handlers/actions/blockActions.js
+++ src/handlers/actions/blockActions.js
@@ -1,5 +1,6 @@
 const queryStrings = require('query-string');
 const moment = require('moment');
+const _ = require('underscore');
 
 const web = require('../../webClient');
 
@@ -13,7 +14,7 @@ const blockActions = async (req, res) => {
   const body = queryStrings.parse(req.body.toString());
   console.log('Received BLOCK ACTIONS', body);
   const payload = JSON.parse(body.payload);
-  const { channel: { id: channel }, user: { id: user }, container: { message_ts: ts }, callback_id, actions: [action] } = payload;
+  const { channel: { id: channel }, user: { id: user }, container: { message_ts: ts }, callback_id, actions: [action], trigger_id } = payload;
   
   console.log(action.action_id, ts);
 
@@ -21,28 +22,8 @@ const blockActions = async (req, res) => {
     case 'no_create_meeting': {
       res.send();
       console.log('remove meeting');
-      // res.send({
-      //   'response_type': 'ephemeral',
-      //   'text': '',
-      //   'replace_original': true,
-      //   'delete_original': true
-      // });
-      // web.chat.delete({ channel, ts });
       break;
     }
-    // case 'yes_create_meeting': {
-    //   console.log('OPEN MEETING DIALOG');
-    //   const { value } = action;
-    //   res.send();
-    //   web.chat.delete({ channel, ts }).then(() => {
-    //     web.chat.postEphemeral({
-    //       channel,
-    //       user,
-    //       text: moment(value).format("dddd, MMMM Do YYYY")
-    //     });
-    //   });
-    //   break;
-    // }
     case 'create_meeting_with_date' : {
       console.log('NEW DATE', action);
       const { selected_date } = action;
@@ -51,25 +32,78 @@ const blockActions = async (req, res) => {
     }
     case 'meeting_menu_options': {
       const { selected_option: { value: meetingId, text: { text } } } = action;
-      if (text == 'Set Reminder') {
-        const meeting = await Meetings.get(meetingId);
+      const meeting = await Meetings.get(meetingId);
+      if (text === 'Set Reminder') {
         console.log('meeting_menu_options Set Reminder', meetingId, meeting);
+        web.dialog.open({
+          trigger_id,
+          dialog: {
+              callback_id: 'set_meeting_reminder',
+              title: 'Set Reminder',
+              submit_label: 'Set',
+              notify_on_cancel: false,
+              state: meeting.id,
+              elements: [{
+                  value: meeting.name,
+                  label: 'Reminder Description',
+                  name: 'reminder_label',
+                  placeholder: meeting.name,
+                  type: 'text',
+                  hint: 'eg. Meeting Title'
+              }, {
+                  label: 'Set Reminder Time',
+                  name: 'reminder_time',
+                  placeholder: 'Choose Reminder Time',
+                  type: 'select',
+                  options: Reminders.getReminderValues(),
+              }]
+            } 
+        });
+        res.send();
+        break;
       }
-      if (text == 'Leave') {
+      if (text === 'View Invite') {
+        web.chat.postEphemeral({
+          channel,
+          user,
+          ...meeting.template
+        }).catch(console.error);
+        res.send();
+        break;
+      }
+      if (text === 'Leave') {
         await Meetings.removeParticipant(meetingId, user);
-        // TODO Remove Reminders & Refactor these common function together
+        await Reminders.removeParticipantReminders({ meetingId, user });
         web.chat.postEphemeral({
           user,
           channel,
           response_type: 'in_channel',
           text: `You've successfully \`Left\` this meeting. Goodbye. 👋`
         });
+        res.send();
+        break;
       }
-      if (text == 'Delete') {
-        web.chat.delete({
+      if (text === 'Delete') {
+        console.log('delete meeting', meetingId);
+        await Reminders.removeMeetingReminders(meetingId);
+        await Meetings.remove(meetingId);
+        const msg = meeting ? `the meeting: *${meeting.name}*` : 'this meeting';
+        web.chat.postEphemeral({
           channel,
           user,
-          ts
+          attachments: [{
+            pretext: `You've successfully \`deleted\` ${msg}. 👋`,
+            callback_id: 'list_meetings',
+            color: '#3AA3E3',
+            attachment_type: 'default',
+            actions: [{
+                name: 'view_all_meetings',
+                value: user,
+                style: 'primary',
+                text: 'View All Meetings',
+                type: 'button'
+            }]
+          }]
         });
       }
       res.send();
@@ -77,16 +111,49 @@ const blockActions = async (req, res) => {
     }
     case 'reminder_options': {
       const { selected_option: { value, text: { text } } } = action;
+      const { channel, scheduled_message_id } = JSON.parse(value);
       
-      if (text === 'Remove Reminder') {
-        const { channel, scheduled_message_id } = JSON.parse(value);
+      if (text === 'Delete Reminder') {
         const response = await Reminders.remove({ user, channel, scheduled_message_id });
         web.chat.postEphemeral(response).catch(console.error);
         res.send();
         break;
       }
       
-      if (text === 'View Meeting') {
+      if (text === 'Edit Reminder') {
+        const reminder = await Reminders.get({ user, scheduled_message_id, channel });
+        if (_.isEmpty(reminder)) {
+          web.chat.postEphemeral({
+            user,
+            channel,
+            text: `⏰ The Reminder may have already \`ended\` or been \`removed\` 👎`
+          });
+          res.send();
+          break;
+        }
+        web.dialog.open({
+          trigger_id,
+          dialog: {
+            callback_id: 'edit_meeting_reminder',
+            title: 'Change Reminder',
+            submit_label: 'Update',
+            notify_on_cancel: false,
+            state: JSON.stringify({ user, scheduled_message_id, channel, meetingId: reminder.meetingId }),
+            elements: [{
+                value: reminder.reminderName,
+                label: 'Reminder Description',
+                name: 'reminder_label',
+                type: 'text',
+                hint: 'eg. Meeting Title'
+            }, {
+                label: 'Set Reminder Time',
+                name: 'reminder_time',
+                value: parseInt(reminder.reminderTime, 10),
+                type: 'select',
+                options: Reminders.getReminderValues()
+            }]
+          } 
+        });
       }
       res.send();
       break;
